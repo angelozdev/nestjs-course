f258273cdbb24f2f539845109db7d073ea29909f
Angelo Zambrano <angelozam17@gmail.com>
2021-09-08 08:12:02 -0500

    :recycle: refactor: single responsability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
+
+// services
 import { AppService } from './app.service'
+
+// controllers
+import { AppController } from './app.controller'
+import { PostsController } from './controllers/posts.controller'
+import { UsersController } from './controllers/users.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, PostsController, UsersController],
   providers: [AppService]
 })
 export class AppModule {}
