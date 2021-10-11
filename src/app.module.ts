414c1b2b6de7a8a69eb7702f91fc674968b973d4
Angelo Zambrano <angelozam17@gmail.com>
2021-10-11 08:03:47 -0500

    :sparkles: feat: add useValue and useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { HttpModule, HttpService } from '@nestjs/axios'
 
 // services
 import { AppService } from './app.service'
@@ -12,8 +13,24 @@ import { UsersModule } from './models/users/users.module'
 import { OrdersModule } from './models/orders/orders.module'
 
 @Module({
-  imports: [ProductsModule, UsersModule, OrdersModule],
+  imports: [ProductsModule, UsersModule, OrdersModule, HttpModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: Math.floor(Math.random() * 10)
+    },
+    {
+      provide: 'user',
+      useFactory: (http: HttpService) => {
+        const usersObservable = http.get('http://localhost:3000/users/1')
+        usersObservable.subscribe(({ data }) => {
+          console.log(data)
+        })
+      },
+      inject: [HttpService]
+    }
+  ]
 })
 export class AppModule {}
